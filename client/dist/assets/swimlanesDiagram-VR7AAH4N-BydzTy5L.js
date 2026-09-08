5d70a1d782858f0ed3169124858edd8f93b2dc45
paresh <mahajanparesh@gmail.com>
2026-09-08 07:40:31 +0530

    fix: remove dist and .env from git tracking and add .gitignore

--- client/dist/assets/swimlanesDiagram-VR7AAH4N-BydzTy5L.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-D_bReoUD.js";import{_ as a}from"./mermaid.core-DCRmcz85.js";import"./chunk-5VM5RSS4-xV6JQaf2.js";import"./chunk-XXDRQBXY-BrSoF0pH.js";import"./chunk-POPQ4Y6H-CxybNiqQ.js";import"./chunk-F27PBJKO-C8jVQyxo.js";import"./channel-SmuCJm_U.js";import"./index-BB7FOEMT.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
